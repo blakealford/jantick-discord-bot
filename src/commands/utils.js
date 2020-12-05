74ad700b7af9747793f529dc60c8d6753a9d8837
SBTrebhuvan <75126357+SBTrebhuvan@users.noreply.github.com>
2020-12-05 16:27:03 +0530

    Create utils.js

---
+++ src/commands/utils.js
@@ -0,0 +1,19 @@
+async function getMemberFromMention(message, mention) {
+  if (!mention) return;
+  const matches = mention.match(/^<@!?(\d+)>$/);
+  if (!matches) return;
+  const id = matches[1];
+  return message.guild.members.cache.get(id);
+}
+
+async function getRolesFromMention(message, mention) {
+  if (!mention) return;
+    const matches = mention.match(/^<@&(\d+)>$/);
+    if (!matches) return;
+    const id = matches[1];
+    return message.guild.roles.cache.get(id);
+}
+module.exports = { 
+  getMemberFromMention,
+  getRolesFromMention
+};
