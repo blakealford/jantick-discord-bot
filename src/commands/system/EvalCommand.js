0f6b24608eb4f1fb4ac51e540c529c76c1d374a0
SBTrebhuvan <75126357+SBTrebhuvan@users.noreply.github.com>
2020-12-05 15:11:01 +0530

    Create EvalCommand.js

---
+++ src/commands/system/EvalCommand.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+const BaseCommand = require('../../utils/structures/BaseCommand');
+module.exports = class GiveRoleCommand extends BaseCommand {
+  constructor() {
+    super('eval', 'System', []);
+  }
+
+  async run(client, message, args) {
+    if(!message.member.roles.cache.find(role => role.id === '778566475489083432') && !message.member.roles.cache.find(role => role.id === '771678863410987018'))
+    const input = args.join(' ');
+    if (!input) return this.sendErrorMessage(message, 0, 'Please provide code to eval');
+    if(!input.toLowerCase().includes('token')) {
+
+      const embed = new MessageEmbed();
+
+      try {
+        let output = eval(input);
+        if (typeof output !== 'string') output = require('util').inspect(output, { depth: 0 });
+        
+        embed
+          .addField('Input', `\`\`\`js\n${input.length > 1024 ? 'Too large to display.' : input}\`\`\``)
+          .addField('Output', `\`\`\`js\n${output.length > 1024 ? 'Too large to display.' : output}\`\`\``)
+          .setColor('#66FF00');
+
+      } catch(err) {
+        embed
+          .addField('Input', `\`\`\`js\n${input.length > 1024 ? 'Too large to display.' : input}\`\`\``)
+          .addField('Output', `\`\`\`js\n${err.length > 1024 ? 'Too large to display.' : err}\`\`\``)
+          .setColor('#FF0000');
+      }
+
+      message.channel.send(embed);
+
+    } else {
+      message.channel.send('(╯°□°)╯︵ ┻━┻ MY token. **MINE**.');
+    }
+  }
+}
